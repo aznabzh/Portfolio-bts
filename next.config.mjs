be569032c1605f12ff1ff3d61bc049d8b0c182cc
Corentin Richard <154679031+aznabzh@users.noreply.github.com>
2026-04-21 22:34:12 +0200

    Update Next.js configuration for export and paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
+  basePath: '/Portfolio-bts',
+  assetPrefix: '/Portfolio-bts',
 }
 
 export default nextConfig
